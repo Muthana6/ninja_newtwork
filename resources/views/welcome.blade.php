1a62cbe01fa3b507b1386f1d0d8c964df0c4e0c8
Muthana6 <marvelge009@gmail.com>
2025-02-07 23:42:48 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Network Ninjas</title>
+</head>
+<body>
+    <h2>Welcome to the Ninja Network</h2>
+    <p> Click the button to view the list of ninjas. </p>
+    <a href="#" class="btn">Find Ninjas</a>
+</body>
+</html>
