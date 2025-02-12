ef374fbc15702b872f076b065da22e4da8cb032b
Muthana6 <marvelge009@gmail.com>
2025-02-12 09:56:48 +0100

    fixed routes mvc

--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -1,3 +1,8 @@
 <x-layout>
-    <h2>Ninja id - {{ $id }}</h2>
+    <h2>{{$ninja ->name}}</h2>
+    <div class="border-gray-200 p-4 rounded">
+        <p><strong>Skill level</strong>{{$ninja -> skill}}</p>
+        <p><strong>About me:</strong></p>
+        <p>{{$ninja->bio}}</p>
+    </div>
 </x-layout>
